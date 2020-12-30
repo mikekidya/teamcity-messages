a16664c5e072aba3ede8e993fc960c2cb40cf2c8
Super user <Super user@TeamCity>
2020-12-30 20:08:25 +0300

    TeamCity change in 'Teamcity Messages' project: build features of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
@@ -30,6 +31,17 @@ changeBuildType(RelativeId("Build")) {
                 provider = github {
                     authType = vcsRoot()
                     filterAuthorRole = PullRequests.GitHubRoleFilter.EVERYBODY
+                }
+            }
+        }
+        add {
+            commitStatusPublisher {
+                vcsRootExtId = "${DslContext.settingsRoot.id}"
+                publisher = github {
+                    githubUrl = "https://api.github.com"
+                    authType = personalToken {
+                        token = "credentialsJSON:9e08e9bf-389e-4df6-9b35-4f3cb488b1dc"
+                    }
                 }
             }
         }
